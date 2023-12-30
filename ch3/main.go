c8d6c938e797a5a9e37b59670ede0f1af65f8727
kamadakohei <kamada.math@gmail.com>
2023-12-31 01:21:56 +0900

    add update

--- ch3/main.go
+++ ch3/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"dbsample/models"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -20,18 +19,40 @@ func main() {
 	}
 	defer db.Close()
 
-	article := models.Article{
-		Title:    "insert test",
-		Contents: "Can I insert data correctly?",
-		UserName: "saki",
-	}
-	const sqlStr = "INSERT INTO articles (title, contents, username, nice, created_at) VALUES (?, ?, ?, 0, now())"
-	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	tx, err := db.Begin()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	article_id := 1
+	const sqlGetNice = `
+	SELECT nice FROM articles WHERE article_id = ?`
 
-	fmt.Println(result.LastInsertId())
-	fmt.Println(result.RowsAffected())
+	row := tx.QueryRow(sqlGetNice, article_id)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	var nicenum int
+	err = row.Scan(&nicenum)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	const sqlUpdateNice = `
+	UPDATE articles SET nice = ? WHERE article_id = ?`
+
+	_, err = tx.Exec(sqlUpdateNice, nicenum+1, article_id)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	tx.Commit()
+
 }
